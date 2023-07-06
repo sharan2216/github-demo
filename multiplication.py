6b975cef3b646982ab94b9589453458714212b41
shashi <sksharan666@gmail.com>
2023-07-06 15:53:42 +0530

    added mutiplication logic

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+# multiplication lines for feature_abc branch
+# multiplication lines for feature_abc branch
+# multiplication lines for feature_abc branch
+
+def mutliplication(a,b):
+    res=a*b
+    print(res)
+
+mutliplication(4,5
+               )
